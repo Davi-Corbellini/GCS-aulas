dc46efe60bfd8e75e381d2d730065c13ef83a87d
Davi Corbellini <davi.corbellini@gmail.com>
2025-06-21 16:27:53 -0300

    adding dotenv

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
-
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: 'localhost',       
-  user: 'root',            
-  password: '1404',   
-  database: 'gcs' 
+  host: process.env.HOST,       
+  user: process.env.USER,            
+  password: process.env.PASSWORD,   
+  database: process.env.DATABASE 
 });
 
 
